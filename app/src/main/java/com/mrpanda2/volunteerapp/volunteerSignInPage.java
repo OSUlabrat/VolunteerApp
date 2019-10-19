a2ed74843f8c5bea4c5eabd9d0a3a739a0780495
Jarod Bucceri <bucceri.8@buckeyemail.osu.edu>
2019-10-18 23:20:47 -0400

    Updated Sign in page to respond to clicks on events button

--- app/src/main/java/com/mrpanda2/volunteerapp/volunteerSignInPage.java
+++ app/src/main/java/com/mrpanda2/volunteerapp/volunteerSignInPage.java
@@ -1,15 +1,26 @@
 package com.mrpanda2.volunteerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class volunteerSignInPage extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
 
+public class volunteerSignInPage extends AppCompatActivity {
+    Button EventsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.volunteer_sign_in);
         Log.d("Fragment Lifecyle", "Working!");
+        EventsButton = findViewById(R.id.button2);
+        EventsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Intent intent = new Intent(volunteerSignInPage.this, EventListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
